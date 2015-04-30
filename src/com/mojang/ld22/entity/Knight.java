e5de6ab1210c8339f58b5ee5210b08bf0252b15a
Gnome Child <kennystepney@hotmail.com>
2015-04-30 15:10:33 -0500

    Update Knight.java

--- src/com/mojang/ld22/entity/Knight.java
+++ src/com/mojang/ld22/entity/Knight.java
@@ -11,4 +11,19 @@ class Knight extends Mob{
   public void tick(){
     super.tick();
   }
+  
+  public int getArmorValue(Material armor){
+	    if(armor == Material.steel){
+	      return 10; 
+	    }
+	    if(armor == Material.reinforced_steel){
+	      return 20; 
+	    }
+	    if(armor == Material.reinforced_iron){
+	      return 15; 
+	    }    
+	    if(armor == Material.iron){
+	      return 5; 
+	    }
+	}
 }
