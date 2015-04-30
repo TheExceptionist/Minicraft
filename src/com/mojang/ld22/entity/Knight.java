9ac8788b089fd6b68a64520282bd264058b59bb5
Gnome Child <kennystepney@hotmail.com>
2015-04-30 14:44:21 -0500

    Update Knight.java

--- src/com/mojang/ld22/entity/Knight.java
+++ src/com/mojang/ld22/entity/Knight.java
@@ -2,7 +2,13 @@ class Knight extends Mob{
   private int lvl;
   private int armorValue;
   
-  public Knight(int lvl){
+  public Knight(int lvl, Material m){
     this.lvl = lvl;
+    //static method in Armor.java
+    armorValue = Armor.getArmorValue(m);
+  }
+  
+  public void tick(){
+    super.tick();
   }
 }
