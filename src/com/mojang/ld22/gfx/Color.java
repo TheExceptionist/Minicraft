8883c40fe087b0f9ea707c5f296311427cbc53af
Gnome Child <kennystepney@hotmail.com>
2015-05-03 08:04:06 -0500

    a lot of math

--- src/com/mojang/ld22/gfx/Color.java
+++ src/com/mojang/ld22/gfx/Color.java
@@ -2,6 +2,8 @@ package com.mojang.ld22.gfx;
 
 public class Color {
 	//Notch made up his custom coloring system, a little confusing at first.
+	//function will accept values 0 to 555
+	//Very confusing but, he use the get method below get(d) for get(a, b, c, d)
 	public static int get(int a, int b, int c, int d) {
 		return (get(d) << 24) + (get(c) << 16) + (get(b) << 8) + (get(a));
 	}
